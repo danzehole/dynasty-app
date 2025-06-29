990b0ddcf485f48f8ce05cc56033711191a982e8
danzehole <Dominik.denzel@googlemail.com>
2025-06-29 19:02:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynasty App
+
+This is a basic Next.js starter app for deployment with Vercel.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>Welcome to Dynasty Tools</h1>
+      <p>This is your deployment-ready starter template.</p>
+    </div>
+  );
+}
