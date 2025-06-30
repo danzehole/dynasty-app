774ce8e693e409a4a07c2f2c8d07e12a19b32f3b
danzehole <Dominik.denzel@googlemail.com>
2025-06-30 20:04:02 +0200

    Add files via upload

---
+++ side-hustle-finder/main.js
@@ -0,0 +1,9 @@
+
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+
+const App = () => {
+  return React.createElement('div', {}, '🚀 Welcome to Side Hustle Finder!');
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
