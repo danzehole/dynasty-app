57e55289dfafaf10455442ead879314e9f74602a
danzehole <Dominik.denzel@googlemail.com>
2025-07-31 21:56:23 +0200

    Delete side-hustle-finder directory

--- side-hustle-finder/main.js
+++
@@ -1,9 +0,0 @@
-
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-
-const App = () => {
-  return React.createElement('div', {}, '🚀 Welcome to Side Hustle Finder!');
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
